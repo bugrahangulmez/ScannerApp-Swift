b9c71347b0352c3f801ea23cde454717e9cac037
bugrahan gulmez <114365074+bugrahangulmez@users.noreply.github.com>
2024-11-07 00:07:13 +0300

    Initial Commit

---
+++ TextRecognationApp/TextRecognationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextRecognationAppApp.swift
+//  TextRecognationApp
+//
+//  Created by Bugrahan on 7.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TextRecognationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
